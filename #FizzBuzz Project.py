f29d18212c6c9bfa18b7efbd7261a1977e679e8f
InnocentBean <124614807+InnocentBean@users.noreply.github.com>
2023-02-06 10:18:07 -0700

    Add files via upload
    #FizzBuzz Project
    num = 0
    while num <= 20:
        num += 1
        if num%3 == 0 and not num%5 == 0:
            print("fizz")
            
        elif num%5 == 0 and not num%3 == 0:
            print("buzz")
            
        elif num%3 == 0 and num%5 == 0:
            print("fizzbuzz") 
            
        else:
            print(num)

---
+++ #FizzBuzz Project.py
@@ -0,0 +1,16 @@
+#FizzBuzz Project
+num = 0
+while num <= 20:
+    num += 1
+    if num%3 == 0 and not num%5 == 0:
+        print("fizz")
+        
+    elif num%5 == 0 and not num%3 == 0:
+        print("buzz")
+        
+    elif num%3 == 0 and num%5 == 0:
+        print("fizzbuzz") 
+        
+    else:
+        print(num)
+        
